93483da519ce165119948833d1920cb7c0fc1cd0
donghar <donghar@naver.com>
2024-09-26 15:57:13 +0900

    test: 회원가입 컨트롤러 테스트 수정
    - 판매자/구매자를 분리하게 되었으며, 더 명확한 앤드 포인트로 수정

--- src/test/java/org/c4marathon/assignment/customer/ui/CustomerControllerTest.java
+++ src/test/java/org/c4marathon/assignment/customer/ui/CustomerControllerTest.java
@@ -1,4 +1,4 @@
-package org.c4marathon.assignment.user.ui;
+package org.c4marathon.assignment.customer.ui;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -6,8 +6,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.UUID;
 
-import org.c4marathon.assignment.user.application.SignUpService;
-import org.c4marathon.assignment.user.ui.dto.request.SignUpRequest;
+import org.c4marathon.assignment.customer.application.SignUpService;
+import org.c4marathon.assignment.customer.ui.dto.request.SignUpRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -20,38 +20,38 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@WebMvcTest(controllers = AuthController.class)
-class AuthControllerTest {
+@WebMvcTest(controllers = CustomerController.class)
+class CustomerControllerTest {
 	@MockBean
 	SignUpService signUpService;
 
 	@Nested
-	@DisplayName("/sign-up")
+	@DisplayName("/customers")
 	class SignUp {
 		@Test
 		@DisplayName("[201]")
 		void shouldReturn201(@Autowired MockMvc mockMvc, @Autowired ObjectMapper objectMapper) throws Exception {
 			UUID nextId = UUID.randomUUID();
 
-			SignUpRequest request = new SignUpRequest("newEmail", "newPassword", "newName", "customer");
+			SignUpRequest request = new SignUpRequest("newEmail", "newPassword", "newName");
 			String expectedMessage = "";
 
-			when(signUpService.registerUser(
-				eq(new SignUpService.Command(request.email(), request.password(), request.name(), request.type()))))
+			when(signUpService.register(
+				eq(new SignUpService.Command(request.email(), request.password(), request.name()))))
 				.thenReturn(nextId);
 
 			ResultActions perform = mockMvc.perform(
-				post("/sign-up")
+				post("/customers")
 					.contentType(MediaType.APPLICATION_JSON)
 					.content(objectMapper.writeValueAsString(request)));
 
 			perform.andExpectAll(
 				status().isCreated(),
-				header().string("Location", "/users/" + nextId),
+				header().string("Location", "/customers/" + nextId),
 				content().string(expectedMessage)
 			);
-			verify(signUpService).registerUser(
-				eq(new SignUpService.Command(request.email(), request.password(), request.name(), request.type())));
+			verify(signUpService).register(
+				eq(new SignUpService.Command(request.email(), request.password(), request.name())));
 		}
 	}
 }
