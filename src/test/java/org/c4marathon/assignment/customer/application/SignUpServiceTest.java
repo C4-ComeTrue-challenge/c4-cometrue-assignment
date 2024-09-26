a7415b4c5f28bfdad2aaba2255affee11a2aa865
donghar <donghar@naver.com>
2024-09-26 16:16:08 +0900

    test: 회원가입 서비스 테스트

---
+++ src/test/java/org/c4marathon/assignment/customer/application/SignUpServiceTest.java
@@ -0,0 +1,41 @@
+package org.c4marathon.assignment.customer.application;
+
+import static org.assertj.core.api.BDDAssertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.UUID;
+
+import org.c4marathon.assignment.customer.domain.Customer;
+import org.c4marathon.assignment.customer.domain.CustomerRepository;
+import org.c4marathon.assignment.customer.domain.PasswordEncoder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SignUpServiceTest {
+	@InjectMocks
+	SignUpService signUpService;
+	@Mock
+	CustomerRepository customerRepository;
+	@Mock
+	PasswordEncoder passwordEncoder;
+
+	@Test
+	@DisplayName("신규 회원 등록시 저장된 회원의 id를 반환한다.")
+	void shouldReturnSavedUserId() {
+		UUID nextId = UUID.randomUUID();
+
+		SignUpService.Command cmd = new SignUpService.Command("newEmail", "newPassword", "newName");
+		when(passwordEncoder.encode(cmd.password())).thenReturn("{noop}" + cmd.password());
+		when(customerRepository.save(refEq(new Customer(cmd.email(), cmd.password(), cmd.name(), passwordEncoder))))
+			.thenReturn(new Customer(nextId, cmd.email(), "{noop}" + cmd.password(), cmd.name()));
+
+		then(signUpService.register(cmd)).isEqualTo(nextId);
+		verify(customerRepository)
+			.save(refEq(new Customer(cmd.email(), cmd.password(), cmd.name(), passwordEncoder)));
+	}
+}
