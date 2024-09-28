29511e094e5db4b46c17f5c2f1feaefa4a107027
donghar <donghar@naver.com>
2024-09-28 22:28:23 +0900

    feat: 회원가입(판매자) 도메인

--- src/test/java/org/c4marathon/assignment/seller/domain/Seller.java
+++ src/test/java/org/c4marathon/assignment/seller/domain/Seller.java
@@ -4,18 +4,56 @@ import java.util.UUID;
 
 import org.c4marathon.assignment.common.entity.Point;
 import org.c4marathon.assignment.customer.domain.PasswordEncoder;
+import org.hibernate.annotations.UuidGenerator;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(
+	name = "SELLERS"
+)
 public class Seller {
+	@Id
+	@GeneratedValue
+	@UuidGenerator(style = UuidGenerator.Style.TIME)
+	private UUID id;
+	@Column(nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String licenseNumber;
+	@Embedded
+	private Point point;
+
+	protected Seller() {
+	}
 
 	Seller(UUID id, String email, String encodedPassword, String name, String licenseNumber, Point point) {
-
+		this.id = id;
+		this.email = email;
+		this.password = encodedPassword;
+		this.name = name;
+		this.licenseNumber = licenseNumber;
+		this.point = point;
 	}
 
 	public Seller(String email, String password, String name, String licenseNumber, PasswordEncoder passwordEncoder) {
-
+		this.email = email;
+		this.password = passwordEncoder.encode(password);
+		this.name = name;
+		this.licenseNumber = licenseNumber;
+		this.point = new Point();
 	}
 
 	public UUID getId() {
-		return null;
+		return id;
 	}
 }
