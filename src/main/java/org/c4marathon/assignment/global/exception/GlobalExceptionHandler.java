b0be872f7406d593f262813d45ece2a70c0324a4
qoraudrb <qoraudrb123@gmail.com>
2024-09-28 15:55:04 +0900

    chore: SQL 예외 추가

--- src/main/java/org/c4marathon/assignment/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/c4marathon/assignment/global/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package org.c4marathon.assignment.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.c4marathon.assignment.global.exception.exceptioncode.ExceptionCode;
 import org.c4marathon.assignment.global.exception.response.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+import static org.c4marathon.assignment.global.exception.exceptioncode.ExceptionCode.INVALID_REQUEST;
 
 @RestControllerAdvice
 @Slf4j
@@ -18,4 +23,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                              .body(new ErrorResponse(e.getCode(), e.getMessage()));
     }
 
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleSQLIntegrityConstraintViolationException(
+            final SQLIntegrityConstraintViolationException e
+    ) {
+        ExceptionCode exceptionCode = INVALID_REQUEST;
+        return ResponseEntity.status(exceptionCode.getHttpStatus())
+                             .body(new ErrorResponse(exceptionCode.getCode(), exceptionCode.getMessage()));
+    }
 }
