9992ce877653b6b5611154f150c14f6fefe5aa19
donghar <donghar@naver.com>
2024-09-29 23:56:20 +0900

    feat: 세션 관련 상수를 관리할 클래스

---
+++ src/main/java/org/c4marathon/assignment/global/session/SessionConst.java
@@ -0,0 +1,8 @@
+package org.c4marathon.assignment.global.session;
+
+public class SessionConst {
+	public static final String SESSION_AUTHENTICATION_PRINCIPAL_KEY = "authentication_principal";
+
+	private SessionConst() {
+	}
+}
