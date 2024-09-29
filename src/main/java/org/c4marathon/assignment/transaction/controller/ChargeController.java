73fbafb8a87f8c6d99986107c351fed19acf239c
qoraudrb <qoraudrb123@gmail.com>
2024-09-29 11:16:26 +0900

    feat: 캐시 충전 기능 구현

---
+++ src/main/java/org/c4marathon/assignment/transaction/controller/ChargeController.java
@@ -0,0 +1,51 @@
+package org.c4marathon.assignment.transaction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.c4marathon.assignment.account.domain.Account;
+import org.c4marathon.assignment.account.service.CommonAccountService;
+import org.c4marathon.assignment.member.domain.MemberAuthority;
+import org.c4marathon.assignment.transaction.dto.ChargeRequest;
+import org.c4marathon.assignment.transaction.service.ChargeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.c4marathon.assignment.member.domain.MemberAuthority.CUSTOMER;
+import static org.c4marathon.assignment.member.domain.MemberAuthority.MERCHANT;
+
+@RestController
+@RequestMapping("/charges")
+@RequiredArgsConstructor
+public class ChargeController {
+
+    private final CommonAccountService commonAccountService;
+    private final ChargeService chargeService;
+
+    @PostMapping
+    public ResponseEntity<Void> chargeAccount(
+            @RequestBody final ChargeRequest chargeRequest,
+            Authentication authentication
+    ) {
+        Account account = getAccount(authentication);
+        chargeService.chargeCash(account.getId(), chargeRequest.money());
+        return ResponseEntity.ok().build();
+    }
+
+    private Account getAccount(Authentication authentication) {
+        Long memAuthId = Long.parseLong(authentication.getName());
+        MemberAuthority authority = getAuthority(authentication);
+        return commonAccountService.findAccountByAuthorityAndMemberAuthId(authority, memAuthId);
+    }
+
+    private static MemberAuthority getAuthority(Authentication authentication) {
+        if (authentication.getAuthorities().contains("MERCHANT")) {
+            return MERCHANT;
+        } else {
+            return CUSTOMER;
+        }
+    }
+}
---
+++ src/main/java/org/c4marathon/assignment/transaction/service/ChargeService.java
@@ -0,0 +1,19 @@
+package org.c4marathon.assignment.transaction.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ChargeService {
+
+    private final TransactionCustomerService transactionCustomerService;
+    private static final Long ADMINISTRATOR_ACCOUNT_ID = 1L;
+
+    @Transactional
+    public void chargeCash(Long toAccountId, Long money) {
+        transactionCustomerService.doTransaction(ADMINISTRATOR_ACCOUNT_ID, toAccountId, money);
+    }
+
+}
