089cd0a8195d9f48ce8a2cec105cf17754e88adb
minsu20 <syt07100@gmail.com>
2024-10-05 19:55:58 +0900

    feat: SecurityConfig 추가

---
+++ src/main/java/org/c4marathon/assignment/global/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package org.c4marathon.assignment.global.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequest ->
+                        authorizeRequest
+                                .requestMatchers(
+                                        AntPathRequestMatcher.antMatcher("/api/auth/**")
+                                ).permitAll()
+                                .requestMatchers(
+                                        AntPathRequestMatcher.antMatcher("/api/check/**")
+                                ).permitAll()
+
+                )
+                .headers(
+                        headersConfigurer ->
+                                headersConfigurer
+                                        .frameOptions(
+                                                HeadersConfigurer.FrameOptionsConfig::sameOrigin
+                                        )
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) ->
+                web
+                        .ignoring()
+                        .requestMatchers(
+                                PathRequest.toStaticResources().atCommonLocations()
+                        );
+    }
+}
