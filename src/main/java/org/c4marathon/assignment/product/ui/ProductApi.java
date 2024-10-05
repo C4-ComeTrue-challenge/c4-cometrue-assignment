1d0975ecd8baf0bee497c6eb248248ab0c4894a1
donghar <donghar@naver.com>
2024-10-05 23:21:53 +0900

    feat: 상품 등록 앤드포인트 생성

---
+++ src/main/java/org/c4marathon/assignment/product/ui/ProductApi.java
@@ -0,0 +1,12 @@
+package org.c4marathon.assignment.product.ui;
+
+import org.c4marathon.assignment.common.authentication.annotation.AuthenticationPrincipal;
+import org.c4marathon.assignment.common.authentication.model.principal.LoginSeller;
+import org.c4marathon.assignment.product.ui.dto.request.RegisterProductRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public interface ProductApi {
+	@PostMapping("/products")
+	ResponseEntity<?> postRegister(@AuthenticationPrincipal LoginSeller loginSeller, RegisterProductRequest request);
+}
