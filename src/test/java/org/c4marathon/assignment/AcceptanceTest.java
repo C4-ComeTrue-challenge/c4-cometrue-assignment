726f4e2e86874c1cae657f04387922673358fa66
donghar <donghar@naver.com>
2024-10-06 12:43:50 +0900

    test: 테스트 환경 분리

--- src/test/java/org/c4marathon/assignment/AcceptanceTest.java
+++ src/test/java/org/c4marathon/assignment/AcceptanceTest.java
@@ -2,10 +2,12 @@ package org.c4marathon.assignment;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-integration.yml")
+@ActiveProfiles("integration")
 @AutoConfigureMockMvc
 public abstract class AcceptanceTest {
 }
