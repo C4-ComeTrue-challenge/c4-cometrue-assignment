b4d88a94710aef09ea29171ba2f023d7b5a9066f
minsu20 <syt07100@gmail.com>
2024-10-06 21:26:13 +0900

    feat: Board DomainService 추가

---
+++ src/main/java/org/c4marathon/assignment/board/domain/service/BoardGetService.java
@@ -0,0 +1,26 @@
+package org.c4marathon.assignment.board.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import org.c4marathon.assignment.board.domain.Board;
+import org.c4marathon.assignment.board.domain.repository.BoardRepository;
+import org.c4marathon.assignment.board.dto.BoardGetAllResponse;
+import org.c4marathon.assignment.board.exception.NotFoundUserException;
+import org.c4marathon.assignment.global.annotation.DomainService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+@DomainService
+@RequiredArgsConstructor
+public class BoardGetService {
+
+    private final BoardRepository boardRepository;
+
+    public Page<BoardGetAllResponse> getAll(Pageable pageable) {
+        return boardRepository.findAllWithPaging(pageable);
+    }
+
+    public Board getById(Long id) {
+        return boardRepository.findById(id)
+                .orElseThrow(() -> new NotFoundUserException());
+    }
+}
