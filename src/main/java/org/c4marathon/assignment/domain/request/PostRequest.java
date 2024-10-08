9c5753ab1a31f1781eca93fb9700c70920bfd1aa
shkum0330 <shkum0330@gmail.com>
2024-10-08 21:27:14 +0900

    test: PostService 테스트 코드 작성

--- src/main/java/org/c4marathon/assignment/domain/request/PostRequest.java
+++ src/main/java/org/c4marathon/assignment/domain/request/PostRequest.java
@@ -6,4 +6,9 @@ import lombok.Data;
 public class PostRequest {
     private String title;
     private String content;
+
+    public PostRequest(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
