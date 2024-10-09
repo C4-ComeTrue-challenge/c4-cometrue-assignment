cfbff281385123c0e7a6623f98ef75cd5b1fdf3b
donghar <donghar@naver.com>
2024-10-09 17:45:15 +0900

    feat: 세션에 카트를 저장하는 레포지토리 구현

--- src/main/java/org/c4marathon/assignment/global/session/SessionConst.java
+++ src/main/java/org/c4marathon/assignment/global/session/SessionConst.java
@@ -2,6 +2,7 @@ package org.c4marathon.assignment.global.session;
 
 public class SessionConst {
 	public static final String SESSION_AUTHENTICATION_PRINCIPAL_KEY = "authentication_principal";
+	public static final String CART_KEY = "cart";
 
 	private SessionConst() {
 	}
