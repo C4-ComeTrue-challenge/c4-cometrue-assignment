661f27151dd4e3f0bea4cdfb110745d58c629bb3
donghar <donghar@naver.com>
2024-10-09 20:12:33 +0900

    fix: Authentication에 Serializable 추가

--- src/main/java/org/c4marathon/assignment/common/authentication/model/Authentication.java
+++ src/main/java/org/c4marathon/assignment/common/authentication/model/Authentication.java
@@ -1,10 +1,11 @@
 package org.c4marathon.assignment.common.authentication.model;
 
+import java.io.Serializable;
 import java.util.Collection;
 
 import org.c4marathon.assignment.common.authentication.model.principal.Principal;
 
-public interface Authentication {
+public interface Authentication extends Serializable {
 	Principal getPrincipal();
 
 	Collection<?> getAuthorities();
