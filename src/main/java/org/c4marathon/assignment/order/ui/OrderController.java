201e2375b2dc9fe48ce25dd75c6e46902e4c5317
donghar <donghar@naver.com>
2024-10-09 17:14:00 +0900

    feat: 카트담기 컨트롤러 구현

---
+++ src/main/java/org/c4marathon/assignment/order/ui/OrderController.java
@@ -0,0 +1,26 @@
+package org.c4marathon.assignment.order.ui;
+
+import java.util.UUID;
+
+import org.c4marathon.assignment.common.authentication.model.principal.LoginCustomer;
+import org.c4marathon.assignment.order.application.AddToCartService;
+import org.c4marathon.assignment.order.ui.dto.request.AddToCartRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController implements OrderApi {
+	private final AddToCartService addToCartService;
+
+	public OrderController(AddToCartService addToCartService) {
+		this.addToCartService = addToCartService;
+	}
+
+	@Override
+	public ResponseEntity<?> postAddToCart(AddToCartRequest request, LoginCustomer loginCustomer) {
+		AddToCartService.Command command = new AddToCartService.Command((UUID)loginCustomer.getId(),
+			request.productId(), request.quantity(), request.version());
+		addToCartService.add(command);
+		return ResponseEntity.ok().build();
+	}
+}
