5bc5450a297f8a941df8172b08fecd41230a0f58
qoraudrb <qoraudrb123@gmail.com>
2024-10-14 20:13:02 +0900

    build: 소나큐브 관련

--- src/test/java/org/c4marathon/assignment/account/service/CommonAccountServiceTest.java
+++ src/test/java/org/c4marathon/assignment/account/service/CommonAccountServiceTest.java
@@ -1,5 +1,7 @@
 package org.c4marathon.assignment.account.service;
 
+import org.c4marathon.assignment.account.domain.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,4 +9,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class CommonAccountServiceTest {
 
+    @Autowired
+    private CommonAccountService commonAccountService;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private
 }
