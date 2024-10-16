6f33deebdaf47f2eb20d5dfd7e99b760ba3be426
minsu20 <syt07100@gmail.com>
2024-10-16 14:55:55 +0900

    fix: DomainService 삭제

--- src/main/java/org/c4marathon/assignment/board/service/BoardService.java
+++ src/main/java/org/c4marathon/assignment/board/service/BoardService.java
@@ -1,76 +1,75 @@
 package org.c4marathon.assignment.board.service;
 
-import lombok.RequiredArgsConstructor;
 import org.c4marathon.assignment.board.domain.Board;
-import org.c4marathon.assignment.board.domain.service.BoardGetService;
-import org.c4marathon.assignment.board.domain.service.BoardSaveService;
+import org.c4marathon.assignment.board.domain.repository.BoardRepository;
 import org.c4marathon.assignment.board.dto.BoardCreateRequest;
 import org.c4marathon.assignment.board.dto.BoardGetAllResponse;
 import org.c4marathon.assignment.board.dto.BoardGetOneResponse;
 import org.c4marathon.assignment.user.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
-    private final BoardSaveService boardSaveService;
-    private final BoardGetService boardGetService;
-    private final BCryptPasswordEncoder encoder;
+	private final BoardRepository boardRepository;
+	private final PasswordEncoder encoder;
 
-    @Transactional
-    public long createBoardAsUser(BoardCreateRequest request, User user) {
-        Board board = toBoard(request, user);
-        return boardSaveService.save(board).getId();
-    }
+	@Transactional
+	public long createBoardAsUser(BoardCreateRequest request, User user) {
+		Board board = toBoard(request, user);
+		return boardRepository.save(board).getId();
+	}
 
-    @Transactional
-    public long createBoardAsGuest(BoardCreateRequest request) {
-        Board board = toBoard(request);
-        return boardSaveService.save(board).getId();
-    }
+	@Transactional
+	public long createBoardAsGuest(BoardCreateRequest request) {
+		Board board = toBoard(request);
+		return boardRepository.save(board).getId();
+	}
 
-    @Transactional(readOnly = true)
-    public Page<BoardGetAllResponse> getAllBoards(Pageable pageable) {
-        return boardGetService.getAll(pageable);
-    }
+	@Transactional(readOnly = true)
+	public Page<BoardGetAllResponse> getAllBoards(Pageable pageable) {
+		return boardRepository.getAll(pageable);
+	}
 
-    @Transactional(readOnly = true)
-    public BoardGetOneResponse getOneBoard(Long id) {
-        Board board = boardGetService.getById(id);
-        return toDto(board);
-    }
+	@Transactional(readOnly = true)
+	public BoardGetOneResponse getOneBoard(Long id) {
+		Board board = boardRepository.getById(id);
+		return toDto(board);
+	}
 
-    private Board toBoard(BoardCreateRequest request, User user) {
-        return Board.builder()
-                .title(request.title())
-                .content(request.content())
-                .user(user)
-                .build();
-    }
+	private Board toBoard(BoardCreateRequest request, User user) {
+		return Board.builder()
+			.title(request.title())
+			.content(request.content())
+			.user(user)
+			.build();
+	}
 
-    private Board toBoard(BoardCreateRequest request) {
-        return Board.builder()
-                .title(request.title())
-                .content(request.content())
-                .writerName(request.writerName())
-                .password(encoder.encode(request.password()))
-                .build();
-    }
+	private Board toBoard(BoardCreateRequest request) {
+		return Board.builder()
+			.title(request.title())
+			.content(request.content())
+			.writerName(request.writerName())
+			.password(encoder.encode(request.password()))
+			.build();
+	}
 
-    private BoardGetOneResponse toDto(Board board) {
-        return new BoardGetOneResponse(
-                board.getId(),
-                board.getContent(),
-                board.getTitle(),
-                board.getWriterName(),
-                board.getCreatedDate(),
-                board.getLastModifiedDate()
-        );
-    }
+	private BoardGetOneResponse toDto(Board board) {
+		return new BoardGetOneResponse(
+			board.getId(),
+			board.getContent(),
+			board.getTitle(),
+			board.getWriterName(),
+			board.getCreatedDate(),
+			board.getLastModifiedDate()
+		);
+	}
 
 }
--- src/main/java/org/c4marathon/assignment/user/service/UserService.java
+++ src/main/java/org/c4marathon/assignment/user/service/UserService.java
@@ -1,10 +1,7 @@
 package org.c4marathon.assignment.user.service;
 
-import lombok.RequiredArgsConstructor;
 import org.c4marathon.assignment.user.domain.User;
-import org.c4marathon.assignment.user.domain.service.UserDeleteService;
-import org.c4marathon.assignment.user.domain.service.UserGetService;
-import org.c4marathon.assignment.user.domain.service.UserSaveService;
+import org.c4marathon.assignment.user.domain.repository.UserRepository;
 import org.c4marathon.assignment.user.dto.EmailCheckResponse;
 import org.c4marathon.assignment.user.dto.LoginRequest;
 import org.c4marathon.assignment.user.dto.NicknameCheckResponse;
@@ -12,71 +9,71 @@ import org.c4marathon.assignment.user.dto.SignupRequest;
 import org.c4marathon.assignment.user.exception.DuplicatedEmailException;
 import org.c4marathon.assignment.user.exception.DuplicatedNicknameException;
 import org.c4marathon.assignment.user.exception.WrongPasswordException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserSaveService saveService;
-    private final UserGetService getService;
-    private final UserDeleteService deleteService;
-    private final BCryptPasswordEncoder encoder;
+	private final UserRepository userRepository;
+	private final PasswordEncoder encoder;
 
-    @Transactional
-    public void signup(SignupRequest request) {
-        validateSignupRequest(request);
-        User user = toUser(request);
-        saveService.save(user);
-    }
+	@Transactional
+	public void signup(SignupRequest request) {
+		validateSignupRequest(request);
+		User user = toUser(request);
+		userRepository.save(user);
+	}
 
-    @Transactional(readOnly = true)
-    public User login(LoginRequest request) {
-        User user = getService.getByEmail(request.email());
-        validatePassword(request.password(), user.getPassword());
-        return user;
-    }
+	@Transactional(readOnly = true)
+	public User login(LoginRequest request) {
+		User user = userRepository.getByEmail(request.email());
+		validatePassword(request.password(), user.getPassword());
+		return user;
+	}
 
-    @Transactional(readOnly = true)
-    public EmailCheckResponse checkEmail(String email) {
-        boolean exists = getService.existByEmail(email);
-        return new EmailCheckResponse(exists);
-    }
+	@Transactional(readOnly = true)
+	public EmailCheckResponse checkEmail(String email) {
+		boolean exists = userRepository.existByEmail(email);
+		return new EmailCheckResponse(exists);
+	}
 
-    @Transactional(readOnly = true)
-    public NicknameCheckResponse checkNickname(String nickname) {
-        boolean exists = getService.existByNickname(nickname);
-        return new NicknameCheckResponse(exists);
-    }
+	@Transactional(readOnly = true)
+	public NicknameCheckResponse checkNickname(String nickname) {
+		boolean exists = userRepository.existByNickname(nickname);
+		return new NicknameCheckResponse(exists);
+	}
 
-    @Transactional
-    public void deleteUser(String email) {
-        User user = getService.getByEmail(email);
-        deleteService.deleteUser(user);
-    }
+	@Transactional
+	public void deleteUser(String email) {
+		User user = userRepository.getByEmail(email);
+		userRepository.deleteUser(user);
+	}
 
-    private void validateSignupRequest(SignupRequest request) {
-        if (getService.existByEmail(request.email())) {
-            throw new DuplicatedEmailException();
-        }
-        if (getService.existByNickname(request.nickname())) {
-            throw new DuplicatedNicknameException();
-        }
-    }
+	private void validateSignupRequest(SignupRequest request) {
+		if (userRepository.existByEmail(request.email())) {
+			throw new DuplicatedEmailException();
+		}
+		if (userRepository.existByNickname(request.nickname())) {
+			throw new DuplicatedNicknameException();
+		}
+	}
 
-    private User toUser(SignupRequest request) {
-        return User.builder()
-                .email(request.email())
-                .password(encoder.encode(request.password()))
-                .nickname(request.nickname())
-                .build();
-    }
+	private User toUser(SignupRequest request) {
+		return User.builder()
+			.email(request.email())
+			.password(encoder.encode(request.password()))
+			.nickname(request.nickname())
+			.build();
+	}
 
-    private void validatePassword(String rawPassword, String encodedPassword) {
-        if (!encoder.matches(rawPassword, encodedPassword)) {
-            throw new WrongPasswordException();
-        }
-    }
+	private void validatePassword(String rawPassword, String encodedPassword) {
+		if (!encoder.matches(rawPassword, encodedPassword)) {
+			throw new WrongPasswordException();
+		}
+	}
 }
