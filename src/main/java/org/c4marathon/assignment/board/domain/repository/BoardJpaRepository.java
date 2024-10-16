e49f19699e7a2a9dfea1a2ce2742b71173d64c56
minsu20 <syt07100@gmail.com>
2024-10-16 14:54:56 +0900

    fix: BoardRepository 이름 변경

---
+++ src/main/java/org/c4marathon/assignment/board/domain/repository/BoardJpaRepository.java
@@ -0,0 +1,8 @@
+package org.c4marathon.assignment.board.domain.repository;
+
+import org.c4marathon.assignment.board.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardJpaRepository extends JpaRepository<Board, Long>, BoardCustomRepository {
+
+}
--- src/main/java/org/c4marathon/assignment/board/domain/repository/BoardRepository.java
+++ src/main/java/org/c4marathon/assignment/board/domain/repository/BoardRepository.java
@@ -1,9 +1,30 @@
 package org.c4marathon.assignment.board.domain.repository;
 
 import org.c4marathon.assignment.board.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.c4marathon.assignment.board.dto.BoardGetAllResponse;
+import org.c4marathon.assignment.board.exception.NotFoundBoardException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
+import lombok.RequiredArgsConstructor;
 
-public interface BoardRepository extends JpaRepository<Board, Long>, BoardCustomRepository {
+@Repository
+@RequiredArgsConstructor
+public class BoardRepository {
 
+	private final BoardJpaRepository boardJpaRepository;
+
+	public Page<BoardGetAllResponse> getAll(Pageable pageable) {
+		return boardJpaRepository.findAllWithPaging(pageable);
+	}
+
+	public Board getById(Long id) {
+		return boardJpaRepository.findById(id)
+			.orElseThrow(() -> new NotFoundBoardException());
+	}
+
+	public Board save(Board board) {
+		return boardJpaRepository.save(board);
+	}
 }
