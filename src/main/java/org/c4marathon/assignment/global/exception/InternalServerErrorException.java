e05ab24fc5ce2dd43edae9a872ffe1bce7305709
minsu20 <syt07100@gmail.com>
2024-10-16 18:28:03 +0900

    fix: 사용하지 않는 파일 삭제

--- src/main/java/org/c4marathon/assignment/global/exception/InternalServerErrorException.java
+++
@@ -1,6 +0,0 @@
-package org.c4marathon.assignment.global.exception;
-
-public class InternalServerErrorException extends RuntimeException{
-
-    public InternalServerErrorException(String message){ super(message); }
-}
