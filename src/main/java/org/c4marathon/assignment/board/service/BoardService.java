0c50e9bdd770b3636fb94e4987ace27bb148597e
opixxx <opix0306@naver.com>
2024-10-16 18:29:02 +0900

    refactor: Repository 분리

--- src/main/java/org/c4marathon/assignment/board/service/BoardService.java
+++ src/main/java/org/c4marathon/assignment/board/service/BoardService.java
@@ -1,14 +1,10 @@
 package org.c4marathon.assignment.board.service;
 
 import lombok.RequiredArgsConstructor;
-import org.c4marathon.assignment.board.domain.Board;
 import org.c4marathon.assignment.board.domain.repository.BoardRepository;
-import org.c4marathon.assignment.board.exception.NotFoundBoardException;
 import org.c4marathon.assignment.board.presentation.dto.BoardResponse;
 import org.c4marathon.assignment.board.service.dto.BoardCreateServiceRequest;
 import org.c4marathon.assignment.board.service.dto.BoardUpdateServiceRequest;
-import org.c4marathon.assignment.global.exception.ErrorCode;
-import org.c4marathon.assignment.user.exception.DuplicateNameException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,43 +17,16 @@ public class BoardService {
     private final BoardRepository boardRepository;
 
     public BoardResponse createBoard(BoardCreateServiceRequest request) {
-
-        if (validateNameDuplicate(request.name())) {
-            throw new DuplicateNameException(ErrorCode.DUPLICATE_NAME);
-        }
-
-        Board board = Board.of(request.name());
-        boardRepository.save(board);
-
-        return new BoardResponse(board.getId(), board.getName());
-    }
-
-    private boolean validateNameDuplicate(String name) {
-        return boardRepository.existsByName(name);
+        return boardRepository.createBoard(request);
     }
 
     @Transactional(readOnly = true)
     public List<BoardResponse> getAllBoard() {
-
-        List<Board> boards = boardRepository.findAll();
-
-        return boards.stream()
-                .map(board -> new BoardResponse(board.getId(), board.getName()))
-                .toList();
+        return boardRepository.getAllBoard();
     }
 
     public BoardResponse updateBoardName(BoardUpdateServiceRequest request) {
-
-        if (validateNameDuplicate(request.name())) {
-            throw new DuplicateNameException(ErrorCode.DUPLICATE_NAME);
-        }
-
-        Board board = boardRepository.findById(request.boardId())
-                .orElseThrow(() -> new NotFoundBoardException(ErrorCode.NOT_FOUND_BOARD));
-
-        board.changeBoardName(request.name());
-
-        return new BoardResponse(board.getId(), board.getName());
+        return boardRepository.updateBoardName(request);
     }
 
     /*public void deleteBoard(BoardDeleteServiceRequest request) {
