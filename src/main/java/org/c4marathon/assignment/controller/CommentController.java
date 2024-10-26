c271302c0f9ff7232fb2b40edffec9074cb29e5d
shkum0330 <shkum0330@gmail.com>
2024-10-26 22:48:51 +0900

    chore: 댓글 엔드포인트 수정

--- src/main/java/org/c4marathon/assignment/controller/CommentController.java
+++ src/main/java/org/c4marathon/assignment/controller/CommentController.java
@@ -9,26 +9,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/posts/{postId}/comment")
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
 
-    @PostMapping("/{postId}/comment")
+    @PostMapping
     public ResponseEntity<CommentResponse> createComment(@PathVariable Long postId, @RequestBody CommentRequest commentRequest,
             HttpSession session) {
         CommentResponse commentResponse = commentService.createComment(postId, commentRequest, session);
         return ResponseEntity.ok(commentResponse);
     }
 
-    @PutMapping("/{postId}/comment/{commentId}")
+    @PutMapping("/{commentId}")
     public ResponseEntity<CommentResponse> updateComment(@PathVariable Long commentId, @RequestBody CommentRequest commentRequest,
             HttpSession session) {
         CommentResponse commentResponse = commentService.updateComment(commentId, commentRequest, session);
         return ResponseEntity.ok(commentResponse);
     }
 
-    @DeleteMapping("/{postId}/comment/{commentId}")
+    @DeleteMapping("/{commentId}")
     public ResponseEntity<?> deleteComment(
             @PathVariable Long commentId, @RequestBody CommentRequest commentRequest, HttpSession session) {
         commentService.deleteComment(commentId,commentRequest ,session);
